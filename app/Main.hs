f483c59b2d150df6eb2ab336db98dcef492a2b42
Ferenc Lukács <flukacs561@gmail.com>
2023-09-28 07:35:26 +0200

    add new commands: nub, note, date

--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,35 @@ import Tableformatter
 
 import System.Environment
 import Data.List ( nub )
+import Data.Time
 
-dispatch :: String -> (String -> String)
+dispatch :: String -> String -> IO ()
 dispatch cmd 
-  | cmd == "ft" = Tableformatter.processTable
-  | cmd == "nub" = unlines . nub . lines
-  | otherwise = throwError cmd
+  | cmd == "ft" = putStrLn . Tableformatter.processTable
+  | cmd == "nub" = putStrLn . unlines . nub . lines
+  | cmd == "note" = note
+  | cmd == "date" = const insertDate
+  | otherwise = putStrLn . throwError cmd
 
 throwError :: String -> String -> String
 throwError cmd _ = "Invalid argument: \"" ++ cmd ++ "\" is not an actual command in hxh."
+
+getCurrentDate :: IO String
+getCurrentDate = do
+    currentTime <- getCurrentTime
+    let formattedDate = formatTime defaultTimeLocale "%Y-%m-%d" currentTime
+    return formattedDate
+
+note :: String -> IO ()
+note title = do
+  date <- getCurrentDate
+  putStrLn $ "---\ntitle: " <> title <> "author: Lukács Ferenc\ndate: " <> date <> "\ntags: \n---"
+
+insertDate :: IO ()
+insertDate = getCurrentDate >>= putStrLn 
 
 main :: IO ()
 main = do
   args <- getArgs
   input <- getContents
-  putStrLn $ dispatch (head args) input
+  dispatch (head args) input
